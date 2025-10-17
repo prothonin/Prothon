9d8918f8e45edc461bc3307b8a703a223b4b2140
prothonin <anshforgame4@gmail.com>
2025-10-17 10:35:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // ✅ Must match your GitHub repo name exactly (case-sensitive)
-  base: "/Prothon/",  
+  base: "/Prothon/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
